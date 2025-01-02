3c53175fcccd202fea4fac7d91a73c6a1f67d1d3
angelwhomst <angelolivarez810@gmail.com>
2025-01-03 00:35:48 +0800

    initial commit

---
+++ DemoLoginApp/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoLoginApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account/Login
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            if (username == "admin" && password == "admin123")
+            {
+                ViewBag.Message = "Login successful!";
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.Message = "Invalid username or password";
+            return View();
+        }
+
+        // GET: Account/Signup
+        [Route("Account/Signup")]
+        public IActionResult Signup()
+        {
+            return View();
+        }
+
+        // POST: Account/Register
+        [HttpPost]
+        [Route("Account/Register")]
+        public IActionResult Register(string username, string password, string confirmPassword)
+        {
+            if (password != confirmPassword)
+            {
+                ViewBag.Message = "Passwords do not match.";
+                return View(); 
+            }
+
+            // Simulate user registration logic
+            ViewBag.Message = "Registration successful!";
+            return RedirectToAction("Login");
+        }
+    }
+}
